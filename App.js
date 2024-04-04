86590d363b2a56bce5714e4ce765621126e59766
Rafael <rafael.y.paul@gmail.com>
2024-04-04 16:34:46 -0300

    st

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View} from "react-native";
-
-import Button from './components/Button'; 
+import { StyleSheet, View } from "react-native";
+import * as MediaLibrary from 'expo-media-library';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import EmojiSticker from './components/EmojiSticker';
+import EmojiList from './components/EmojiList';
+import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
 import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
-
+import { useState, useRef } from 'react';
+import EmojiPicker from "./components/EmojiPicker";
+import CircleButton from './components/CircleButton';
+import IconButton from './components/IconButton';
 const PlaceholderImage = require("./assets/images/background-image.png");
+import { captureRef } from 'react-native-view-shot';
 
 
 
 export default function App() {
+  const imageRef = useRef();
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+  const [pickedEmoji, setPickedEmoji] = useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
   const [showAppOptions, setShowAppOptions] = useState(false);
+
+  if (status === null) {
+    requestPermission();
+  }
+
+  
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -33,37 +49,48 @@ export default function App() {
   };
 
   const onAddSticker = () => {
-    // we will implement this later
+    setIsModalVisible(true);
   };
 
   const onSaveImageAsync = async () => {
     // we will implement this later
   };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-      <ImageViewer
-          placeholderImageSource={PlaceholderImage}
-          selectedImage={selectedImage}
-        />
-      </View>{showAppOptions ? (
-        <View style={styles.optionsContainer}>
-        <View style={styles.optionsRow}>
-          <IconButton icon="refresh" label="Reset" onPress={onReset} />
-          <CircleButton onPress={onAddSticker} />
-          <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
-        </View>
-      </View>
-      ) : (
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-        <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
 
-      </View>
-      )}
-      <StatusBar style="auto" />
-    </View>
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <View style={styles.container}>
+      <View ref={imageRef} collapsable={false}>
+          <ImageViewer
+            placeholderImageSource={PlaceholderImage}
+            selectedImage={selectedImage}
+
+          /> {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
+        </View>
+        {showAppOptions ? (
+          <View style={styles.optionsContainer}>
+            <View style={styles.optionsRow}>
+              <IconButton icon="refresh" label="Reset" onPress={onReset} />
+              <CircleButton onPress={onAddSticker} />
+              <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+            </View>
+          </View>
+        ) : (
+          <View style={styles.footerContainer}>
+            <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+            <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+
+          </View>
+        )
+        }
+        <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+          <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+        </EmojiPicker>
+        <StatusBar style="auto" />
+      </View></GestureHandlerRootView>
   );
 }
 
